6d23d16ffd726510595f99d33286716edd2fbca9
Matthew <matthewdavidr12@hotmail.com>
2026-01-14 16:02:56 -0600

    GOD HELP

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+console.log("API BASE =", import.meta.env.VITE_API_BASE); // 👈 ADD THIS
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
